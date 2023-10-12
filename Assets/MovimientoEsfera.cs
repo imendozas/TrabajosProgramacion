7c741b96f0e3cc7d4df3095c2eaef6d977127887
imendozas <i.mendozas@cocoschool.mx>
2023-10-12 15:07:12 -0600

    BALAS

---
+++ Assets/MovimientoEsfera.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MovimientoEsfera : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Translate(Vector3.right * speed  * Time.deltaTime);
+
+    
+
+
+
+
+
+    }
+    
+}
